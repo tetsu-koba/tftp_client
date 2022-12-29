b96ea8c86feaf6480871f5b073f9060b5b8bc4c9
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2022-12-29 10:32:04 +0900

    Add D for debug print

--- tftp_client.zig
+++ tftp_client.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const os = std.os;
-const debug = std.debug;
 const time = std.time;
 const net = std.net;
 const mem = std.mem;
@@ -17,7 +16,7 @@ const opcode = struct {
     const ERROR = 5;
 };
 
-fn makeReq(buf: []u8, opc:u16, remotename: []const u8) !usize {
+fn makeReq(buf: []u8, opc: u16, remotename: []const u8) !usize {
     var fbs = std.io.fixedBufferStream(buf);
     const w = fbs.writer();
 
@@ -41,9 +40,20 @@ fn checkDataHead(b: []u8, n: u16) bool {
     return true;
 }
 
+const D = struct {
+    v: bool,
+    const Self = @This();
+    fn print(self: Self, comptime fmt: []const u8, a: anytype) void {
+        if (self.v) {
+            std.debug.print(fmt, a);
+        }
+    }
+};
+
 var payload_buf: [UDP_PAYLOADSIZE]u8 = undefined;
 
 pub fn tftpRead(adr: []const u8, port: u16, remotename: []const u8, s: *std.io.StreamSource, timeout: i32, verbose: bool) !void {
+    const d: D = .{ .v = verbose };
     const w = s.writer();
     const sockfd = try os.socket(os.AF.INET, os.SOCK.DGRAM | os.SOCK.CLOEXEC, 0);
     defer os.closeSocket(sockfd);
@@ -51,7 +61,7 @@ pub fn tftpRead(adr: []const u8, port: u16, remotename: []const u8, s: *std.io.S
     const a = try net.Address.resolveIp(adr, port);
     var svraddr: std.os.linux.sockaddr = undefined;
     var svraddrlen: std.os.socklen_t = @sizeOf(os.linux.sockaddr);
-    var bytes_read: usize = 0;
+    var recv_bytes: usize = 0;
     var block_n: u16 = 0;
     var pfd = [1]os.pollfd{.{
         .fd = sockfd,
@@ -61,9 +71,7 @@ pub fn tftpRead(adr: []const u8, port: u16, remotename: []const u8, s: *std.io.S
     var retry_count: u16 = 0;
     while (retry_count < RETRY_MAX) : (retry_count += 1) {
         const send_bytes = try os.sendto(sockfd, req, 0, &a.any, a.getOsSockLen());
-        if (verbose) {
-            debug.print("{d}:send_bytes={d}, a={}", .{ time.milliTimestamp(), send_bytes, a });
-        }
+        d.print("{d}:send_bytes={d}, a={}\n", .{ time.milliTimestamp(), send_bytes, a });
         const nevent = os.poll(&pfd, timeout) catch 0;
         if (nevent == 0) {
             // timeout
@@ -73,15 +81,10 @@ pub fn tftpRead(adr: []const u8, port: u16, remotename: []const u8, s: *std.io.S
             std.log.err("{d}:Got revents={d}", .{ time.milliTimestamp(), pfd[0].revents });
             return os.ReadError.ReadError;
         }
-        bytes_read = try os.recvfrom(sockfd, &payload_buf, 0, &svraddr, &svraddrlen);
-        if (verbose) {
-            debug.print("{d}:bytes_read={d} {} [{s}].", .{ time.milliTimestamp(), bytes_read, svraddr, payload_buf[0..bytes_read] });
-            // var out_buf: [1024]u8 = undefined;
-            // const n = try toStr(payload_buf[0..bytes_read], &out_buf);
-            // debug.print("len = {d}, s=[{s}]\n", .{ n, out_buf[0..n] });
-        }
+        recv_bytes = try os.recvfrom(sockfd, &payload_buf, 0, &svraddr, &svraddrlen);
+        d.print("{d}:recv_bytes={d} {}\n", .{ time.milliTimestamp(), recv_bytes, svraddr });
         if (checkDataHead(payload_buf[0..4], 1)) {
-            _ = try w.writeAll(payload_buf[4..bytes_read]);
+            _ = try w.writeAll(payload_buf[4..recv_bytes]);
             block_n = 1;
             break;
         }
@@ -90,18 +93,14 @@ pub fn tftpRead(adr: []const u8, port: u16, remotename: []const u8, s: *std.io.S
     }
 
     try os.connect(sockfd, &svraddr, svraddrlen);
-    if (verbose) {
-        debug.print("{d}:connect, a={}", .{ time.milliTimestamp(), svraddr });
-    }
+    d.print("{d}:connect, a={}\n", .{ time.milliTimestamp(), svraddr });
     var ack: []u8 = undefined;
     retry_count = 0;
     while (retry_count < RETRY_MAX) {
         ack = payload_buf[0..makeAck(&payload_buf, block_n)];
         const send_bytes = try os.send(sockfd, ack, 0);
-        if (verbose) {
-            debug.print("{d}:send_bytes={d} block_n={d}", .{ time.milliTimestamp(), send_bytes, block_n });
-        }
-        if (bytes_read < DATA_MAXSIZE) {
+        d.print("{d}:send_bytes={d} block_n={d}\n", .{ time.milliTimestamp(), send_bytes, block_n });
+        if (recv_bytes < DATA_MAXSIZE) {
             return;
         }
         const nevent = os.poll(&pfd, timeout) catch 0;
@@ -114,12 +113,10 @@ pub fn tftpRead(adr: []const u8, port: u16, remotename: []const u8, s: *std.io.S
             std.log.err("{d}:Got revents={d}", .{ time.milliTimestamp(), pfd[0].revents });
             return os.ReadError.ReadError;
         }
-        bytes_read = try os.recv(sockfd, &payload_buf, 0);
-        if (verbose) {
-            debug.print("{d}:bytes_read={d} {d}", .{ time.milliTimestamp(), bytes_read, payload_buf[3] });
-        }
+        recv_bytes = try os.recv(sockfd, &payload_buf, 0);
+        d.print("{d}:recv_bytes={d} {d}\n", .{ time.milliTimestamp(), recv_bytes, payload_buf[3] });
         if (checkDataHead(payload_buf[0..4], block_n + 1)) {
-            _ = try w.writeAll(payload_buf[4..bytes_read]);
+            _ = try w.writeAll(payload_buf[4..recv_bytes]);
             block_n += 1;
             retry_count = 0;
             continue;
@@ -129,4 +126,3 @@ pub fn tftpRead(adr: []const u8, port: u16, remotename: []const u8, s: *std.io.S
         return os.ReadError.ReadError;
     }
 }
-
